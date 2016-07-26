f69ee6c6362e5d3d944636288ee54468a26a73a9
David <davidholder24@gmail.com>
2016-07-26 23:47:49 +0100

    usernames and adding stories

--- app.js
+++ app.js
@@ -13,19 +13,77 @@ console.log("Server started.");
 
 var SOCKET_LIST = {};
 
+// socketid: socket.id, username: username.value
+var activeAccounts = [];
+
+var USERS = {
+	//username: password
+	'david': 'Iceland18'
+}
+
+var STORIES = []
+
+var isValidPassword = function(data) {
+	return USERS[data.username] === data.password;
+}
+
+var isUsernameTaken = function(data) {
+	return USERS[data.username];
+}
+
+var addUser = function(data) {
+	USERS[data.username] = data.password;
+}
+
+var addActiveAccount = function(socketid, username) {
+	var pack = {};
+	pack.socketid = socketid;
+	pack.username = username;
+	activeAccounts.push(pack);
+}
+
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
     socket.id = Math.random();
     SOCKET_LIST[socket.id] = socket;
+
+    socket.on('signIn', function(data) {
+    	if (isValidPassword(data)) {
+    		addActiveAccount(socket.id, data.username);
+    		socket.emit('signInResponse', {success:true});
+    	} else {
+    		socket.emit('signInResponse', {success:false});
+    	}
+    	
+    });
+
+    socket.on('signUp', function(data){
+    	if(isUsernameTaken(data)){
+    		socket.emit('signUpResponse', {success:false});
+    	} else {
+    		addUser(data);
+    		socket.emit('signUpResponse', {success:true});
+    	}
+    });
     
     socket.on('disconnect',function(){
         delete SOCKET_LIST[socket.id];
     });
     socket.on('sendMsgToServer',function(data){
-        var playerName = ("" + socket.id).slice(2,7);
-        for(var i in SOCKET_LIST){
-            SOCKET_LIST[i].emit('addToChat',playerName + ': ' + data);
+        var  playerName = '';
+        if (data.username === 'Guest') {
+        	var guestUserName = ("" + socket.id).slice(2,7);
+        	playerName = "Guest" + guestUserName;
+        } else {
+        	playerName = data.username;
         }
+        for(var i in SOCKET_LIST){
+            SOCKET_LIST[i].emit('addToChat',playerName + ': ' + data.chattext);
+        }
+    });
+
+    socket.on('storyUpload',function(data){
+        STORIES.push(data);
     });
    
 });
--- client/website.js
+++ client/website.js
@@ -1,5 +1,8 @@
+// Variables  saving who you are
+var username = 'Guest';
+
 // Server to  client and client to server
- var socket = io();
+var socket = io();
 
 // Chat
 var chatText = document.getElementById('chat-text');
@@ -9,22 +12,73 @@ var chatForm = document.getElementById('chat-form');
 
 socket.on('addToChat',function(data){
     chatText.innerHTML += '<div class="chat-inner-window-sub">' + data + '</div>';
+    windowScroll();
 });
 
 chatForm.onsubmit = function(e){
     e.preventDefault();
-	socket.emit('sendMsgToServer',chatInput.value);
+	socket.emit('sendMsgToServer',{chattext: chatInput.value, username: username});
     chatInput.value = '';      
 }
 
 // Update Chat Scroll
 
+var windowScroll = function() {
+  	var elem = document.getElementById('chat-text');
+  	elem.scrollTop = elem.scrollHeight;
+	};
 
-//  Website Jquery
+// Sign in Server Update
+
+var signDiv = document.getElementById('signDiv');
+var signDivUsername = document.getElementById('signDiv-username');
+var signDivSignIn = document.getElementById('signDiv-signIn');
+var signDivSignUp = document.getElementById('signDiv-signUp');
+var signDivPassword = document.getElementById('signDiv-password');
+
+signDivSignIn.onclick = function(){
+	username = signDivUsername.value;
+    socket.emit('signIn',{username:signDivUsername.value,password:signDivPassword.value});
+    signDivUsername.value = '';
+    signDivPassword.value = '';
+}
+signDivSignUp.onclick = function(){
+    socket.emit('signUp',{username:signDivUsername.value,password:signDivPassword.value});
+}
+socket.on('signInResponse',function(data){
+	var welcome = document.getElementById('welcome'); 
+    if(data.success){
+        welcome.innerHTML = 'Welcome ' + username + ', you are now logged in';
+    } else
+        alert("Sign in unsuccessful.");
+});
+socket.on('signUpResponse',function(data){
+    if(data.success){
+        alert("Sign up successful.");
+    } else
+        alert("Sign up unsuccessful.");
+});
+
+// Uploading a Story
+
+var title = document.getElementById('form-story-title');
+var content = document.getElementById('form-link-content');
+var submitStory = document.getElementById('submitStory');
+
+submitStory.onclick = function() {
+	if (username != 'Guest') {
+		socket.emit('storyUpload', {username: username, title: title.value, content: content.value, comments: '', likes: 0});
+		title.value = '';
+		content.value = '';
+	} else {
+		alert('You need to be logged in to upload a story! Please sign up, its quick, easy and free!');
+	}
+}
+
+// Website Jquery
 function authorShow(author, jsauthor) {
 	$(author).on('click', function(event) {
     	event.preventDefault();
-
     	$(jsauthor).addClass('is-visible');
     	$('.js-modal-overlay').addClass('is-visible');
   	});
@@ -49,6 +103,18 @@ $(document).ready(function() {
 		$('.js-form').toggleClass('is-visible');
 	});
 
+	// Sign In Animation
+
+	$('.signIn-wrapper').on('click', function(event) {
+		event.preventDefault();
+		$('.js-SignIn').toggleClass('chat-visible').children().click(function(e) {
+  			return false;
+		});
+		$('.chat-wrapper').toggleClass('hidden');
+		$('.lucy-chat-button').toggleClass('big-font');
+		$('.lucy-chat-button').toggleClass('small-font');
+	});
+
 	// Chat
 
 	$('.chat-wrapper').on('click', function(event) {
@@ -56,14 +122,12 @@ $(document).ready(function() {
 		$('.js-chat').toggleClass('chat-visible').children().click(function(e) {
   			return false;
 		});
+		$('.signIn-wrapper').toggleClass('hidden');
 		$('.lucy-chat-button').toggleClass('big-font');
 		$('.lucy-chat-button').toggleClass('small-font');
 	});
 
-	window.setInterval(function() {
-  	var elem = document.getElementById('chat-text');
-  	elem.scrollTop = elem.scrollHeight;
-	}, 500);
+	
 
 	// Modal
 	authorShow('.david', '.js-david');
